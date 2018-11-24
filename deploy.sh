bd8b1ea88d86bbc4c589ab6a2c9d4060bff99f99
Pedro Duarte <xipasduarte@gmail.com>
2018-11-24 20:21:14 +0000

    Final corrections.

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,17 @@
 # Cleanup (for those who don't use a CI).
 rm -rf public
 
-# Who am I?
-git config --global user.email "${GIT_USER_EMAIL}"
-git config --global user.name "${GIT_USER_NAME}"
-
 # Clone original repository.
 git clone $(git remote get-url origin) public
 
 
 # Attempt to switch to the deployment branch.
 cd public/
+
+# Who am I?
+git config user.email "${GIT_USER_EMAIL}"
+git config user.name "${GIT_USER_PASSWORD}"
+
 git checkout gh-pages
 if [ 0 != $? ]; then
   # Create a new orphan branch to track deployments
